bfdd2650bd575bd3aabab429ab1f5b4e1a60cfb2
Hridoy Hasan <hridoynasah@gmail.com>
2025-05-22 01:44:01 +0600

    Refactor code structure for improved readability and maintainability

---
+++ WEEK01/file handling/2_mode_w.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    FILE * fp = NULL;
+
+    // Opens the file for writing. Erases existing contents, creating a new empty file.
+    fp = fopen("Hridoy.txt", "w");
+
+    if(fp == NULL)
+    {
+        printf("Error opening file...\n");
+    }
+    else
+    {
+        printf("File opened successfully.");
+    }
+
+    return 0;
+}
