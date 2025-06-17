90b8ca801c8bf5b9c7a9fae52b3bb316c6250daf
Hridoy Hasan <hridoynasah@gmail.com>
2025-06-17 23:45:59 +0600

    added new problem

---
+++ WEEK01/LAB01/Problem_06.cpp
@@ -0,0 +1,16 @@
+// Problem 06: Write a program to count the total number of alphabets, digits and special characters in a string. 
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    char str[100];
+    int i = 0;
+    int alpha = 0, dig = 0, sp = 0;
+    while(str[i] != '\0'){
+        if(str[i] >= 'A' && str[i] <= 'Z' || str[i] >= 'a' && str[i] <= 'z')
+    }
+
+    return 0;
+}
