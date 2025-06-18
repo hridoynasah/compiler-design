618f295ab602dce30354ab86269673643f9977d0
Hridoy Hasan <hridoynasah@gmail.com>
2025-06-18 11:49:09 +0600

    changed

--- WEEK01/LAB01/Problem_06.cpp
+++ WEEK01/LAB01/Problem_06.cpp
@@ -6,11 +6,27 @@ using namespace std;
 int main(){
 
     char str[100];
+    cout << "Enter the input: ";
+    cin.getline(str, 100);
+
     int i = 0;
     int alpha = 0, dig = 0, sp = 0;
     while(str[i] != '\0'){
-        if(str[i] >= 'A' && str[i] <= 'Z' || str[i] >= 'a' && str[i] <= 'z')
+        if(str[i] >= 'A' && str[i] <= 'Z' || str[i] >= 'a' && str[i] <= 'z'){
+            alpha++;
+        }
+        else if(str[i] >= '0' && str[i] <= '9'){
+            dig++;
+        }
+        else{
+            sp++;
+        }
+        i++;
     }
+
+    cout << "Alphabets: " << alpha << endl;
+    cout << "Digits: " << dig << endl;
+    cout << "Special Characters: " << sp << endl;
 
     return 0;
 }
